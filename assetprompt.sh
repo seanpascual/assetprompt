7c697045d86fbdc0908a2d6a25415d290b15702a
Sean Pascual <seanpascual@gmail.com>
2017-09-18 14:16:50 +0100

    Added exit status. Added skip of apps, fonts, and plugins during recon.

--- assetprompt.sh
+++ assetprompt.sh
@@ -9,23 +9,25 @@
 REPEAT=true
 while ${REPEAT}; do
 
-    COMPNAME="$(osascript -e 'display dialog "Enter asset tag 4 digit number" with icon caution default answer "" buttons{"Continue"} default button "Continue"')"
+COMPNAME="$(osascript -e 'display dialog "Enter asset tag 4 digit number" with icon caution default answer "" buttons{"Continue"} default button "Continue"')"
 
 
-    COMPNAME="$(echo $COMPNAME | cut -c 41-44)"
-    ASSETTAG="${COMPNAME}"
-    COMPNAME="Beamly-$COMPNAME"
+COMPNAME="$(echo $COMPNAME | cut -c 41-44)"
+ASSETTAG="${COMPNAME}"
+COMPNAME="Beamly-$COMPNAME"
 
-    DISPLAYNAME="Are you sure you want to name this computer: $COMPNAME"
+DISPLAYNAME="Are you sure you want to name this computer: $COMPNAME"
 
-    CONFIRM=$(osascript -e "display dialog \"${DISPLAYNAME}\" buttons {\"Yes\", \"No\"} default button \"No\"")
+CONFIRM=$(osascript -e "display dialog \"${DISPLAYNAME}\" buttons {\"Yes\", \"No\"} default button \"No\"")
 
-    if [[ "${CONFIRM}" == "button returned:Yes" ]]; then
-        echo "Setting name to ${COMPNAME}"
-        jamf recon -verbose -assetTag "${ASSETTAG}"
-        scutil --set ComputerName $COMPNAME
-        scutil --set HostName $COMPNAME
-        scutil --set LocalHostName $COMPNAME
-        REPEAT=false
-    fi
+if [[ "${CONFIRM}" == "button returned:Yes" ]]; then
+#echo "Setting name to ${COMPNAME}"
+jamf recon -skipApps -skipFonts -skipPlugins -assetTag ${ASSETTAG}
+scutil --set ComputerName $COMPNAME
+scutil --set HostName $COMPNAME
+scutil --set LocalHostName $COMPNAME
+REPEAT=false
+fi
 done
+
+exit 0
