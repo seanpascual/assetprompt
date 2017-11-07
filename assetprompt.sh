be4d0b1d634cf8641b2b885da441d3b5c1d74171
Sean Pascual <seanpascual@gmail.com>
2017-11-07 12:39:05 +0000

    Added LDAP integration for full name search

--- assetprompt.sh
+++ assetprompt.sh
@@ -11,13 +11,19 @@
 # DEPARTMENTS TO BE HIDDEN FROM USER - EDIT AS NECESSARY
 declare -a DEPTSTOREMOVE=('_LOST/STOLEN' '_OUTFORREPAIR' '_SPARE')
 
+# LDAP USERS TO BE HIDDEN FROM USER - EDIT AS NECESSARY
+declare -a NAMESTOREMOVE=('ldapadmin')
+
 user='ls -la /dev/console | cut -d " " -f 4'
 
 USERNAME="$4"
 PASS="$7"
 JSSPATH="$8"
 
-# SET NAME OF COMPUTER
+###
+### SET NAME OF COMPUTER
+###
+
 REPEAT=true
 while ${REPEAT}; do
 
@@ -38,7 +44,9 @@ REPEAT=false
 fi
 done
 
+###
 ### SET DEPARTMENT FOR COMPUTER
+###
 
 # CURL DEPARTMENT LIST FROM THE JSS
 DEPTS=$(curl -H "Accept: application/json" -u $USERNAME:$PASS $JSSPATH/departments | sed -e 's/[{}]/''/g' |  awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep "name" | while read line; do cut -c 8- | sed 's/]//g'; done)
@@ -67,19 +75,68 @@ EOF)
 echo ${DEPTNAME}
 done
 
-# SET FULL NAME OF USER OF COMPUTER
-FULLNAME=""
-while [ "${FULLNAME}" == "" ]; do
-FULLNAME="$(osascript -e 'display dialog "Enter the first name and surname of the user of this computer" default answer "" buttons{"Continue"} default button "Continue"')"
-FULLNAME="$(echo ${FULLNAME} | cut -c 41-)"
+###
+### SET FULL NAME OF USER OF COMPUTER
+###
+
+REPEATFULLNAME=true
+while ${REPEATFULLNAME}; do
+
+LASTNAME="$(osascript -e 'display dialog "Enter the surname of the user of this computer" default answer "" buttons{"Continue"} default button "Continue"')"
+LASTNAME="$(echo ${LASTNAME} | cut -c 41-)"
+NAMES=$(ldapsearch -ZZ -LLL -b "dc=beamly,dc=internal" -D "uid=authenticate,ou=system,dc=beamly,dc=internal" -w 'b1nder!' -H ldap://externalldap.beamly.com cn | grep -v "dn:" | grep -i "${LASTNAME}" | cut -c 5-)
+
+# ITERATE THROUGH AND REMOVE NAMES TO NOT SHOW TO USER FROM 'NAMESTOREMOVE' VAR ABOVE
+for i in ${NAMESTOREMOVE[@]}; do
+NAMES=$(echo "${NAMES]" | grep -v "${i}")
 done
 
-# SET EMAIL ADDRESS OF USER OF COMPUTER
-EMAIL=""
-while [ "${EMAIL}" == "" ]; do
-EMAIL="$(osascript -e 'display dialog "Enter the email address of the user of this computer" default answer "" buttons{"Continue"} default button "Continue"')"
-EMAIL="$(echo ${EMAIL}  | cut -c 41-)"
+# CHANGE NAME DATA TO SOMETHING APPLESCRIPT CAN READ
+NAMES=$(echo ${NAMES} | sed 's/$/,/')
+NAMES=${NAMES%?}
+
+# DISPLAY DIALOG TO USER TO CHOOSE THEIR NAME
+FULLNAME="false"
+
+while [ ${FULLNAME} == "false" ]; do
+FULLNAME=$(/usr/bin/osascript << EOF
+set nameList to {$NAMES}
+choose from list nameList with prompt "Select your name from below"
+EOF)
+echo ${FULLNAME}
 done
+
+# CONFIRM USER WANTS TO USE THIS NAME
+DISPLAYFULLNAME="Confirm that your name is: $FULLNAME"
+
+CONFIRMFULLNAME=$(osascript -e "display dialog \"${DISPLAYFULLNAME}\" buttons {\"Yes\", \"No\"} default button \"No\"")
+
+if [[ "${CONFIRMFULLNAME}" == "button returned:Yes" ]]; then
+REPEATFULLNAME=false
+
+fi
+done
+
+#FULLNAME=""
+#while [ "${FULLNAME}" == "" ]; do
+#FULLNAME="$(osascript -e 'display dialog "Enter the first name and surname of the user of this computer" default answer "" buttons{"Continue"} default button "Continue"')"
+#FULLNAME="$(echo ${FULLNAME} | cut -c 41-)"
+#done
+
+###
+### SET EMAIL ADDRESS OF USER OF COMPUTER
+###
+
+# CONVERT PREVIOUSLY SEARCHED NAME TO UID
+SEARCHFULLNAME=$(echo ${FULLNAME} | sed 's/[[:space:]]/./')
+
+EMAIL=$(ldapsearch -ZZ -LLL -b "dc=beamly,dc=internal" -D "uid=authenticate,ou=system,dc=beamly,dc=internal" -w 'b1nder!' -H ldap://externalldap.beamly.com mail | grep -v "dn:" | grep -i "${SEARCHFULLNAME}" | cut -c 7-)
+
+#EMAIL=""
+#while [ "${EMAIL}" == "" ]; do
+#EMAIL="$(osascript -e 'display dialog "Enter the email address of the user of this computer" default answer "" buttons{"Continue"} default button "Continue"')"
+#EMAIL="$(echo ${EMAIL} | cut -c 41-)"
+#done
 
 #echo "Setting name to ${COMPNAME}"
 scutil --set ComputerName $COMPNAME
